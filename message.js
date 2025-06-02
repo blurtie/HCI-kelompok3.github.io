d48a4bac1c42f8fd660711497a2f261e5258ee1d
Muhammad Faiqi Harumantaka <88717555+blurtie@users.noreply.github.com>
2025-06-02 21:49:44 +0700

    Add files via upload

---
+++ message.js
@@ -0,0 +1,27 @@
+const read = document.querySelector('.read');
+const message = document.querySelector('.msg');
+
+message.addEventListener('click', () => {
+    read.style.display = read.style.display === "none" ? "block" : "none";
+});
+
+const read1 = document.querySelector('.read1');
+const message1 = document.querySelector('.msg1');
+
+message1.addEventListener('click', () => {
+    read1.style.display = read1.style.display === "none" ? "block" : "none";
+});
+
+const read2 = document.querySelector('.read2');
+const message2 = document.querySelector('.msg2');
+
+message2.addEventListener('click', () => {
+    read2.style.display = read2.style.display === "none" ? "block" : "none";
+});
+
+const read3 = document.querySelector('.read3');
+const message3 = document.querySelector('.msg3');
+
+message3.addEventListener('click', () => {
+    read3.style.display = read3.style.display === "none" ? "block" : "none";
+});
---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+      const menu = document.getElementById('acc-option');
+      menu.classList.toggle('show');
+    }
